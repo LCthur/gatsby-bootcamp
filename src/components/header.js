59baef43c6719d21da1779298b0dbff922089458
Loic Thurre <loic.thurre@gmail.com>
2019-08-29 14:37:50 +0200

    css modules are done, basics are learned let's go for the data mangement with graphQl

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,38 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import headerStyles from'./header.module.scss'
+
 const Header = () => {
     return(
-        <header>
-            <h1>Loïc Thurre's first gatsby site</h1>
+        <header className={headerStyles.header} >
+            <h1>
+                <Link className={headerStyles.title} to="/">
+                    Loïc Thurre
+                </Link>
+            </h1>
             <nav>
-                <ul>
-                    <li><Link to="/contact">Contact</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/blog">Blog</Link></li>  
-                    <li><Link to="/">Homepage</Link></li>
+                <ul className={headerStyles.navList}> 
+                    <li>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/contact">
+                            Contact
+                        </Link>
+                    </li>
+                    <li>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/about">
+                            About
+                        </Link>
+                    </li>
+                    <li>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/blog">
+                            Blog
+                        </Link>
+                    </li>  
+                    <li>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/">
+                            Homepage
+                        </Link>
+                    </li>
                 </ul>
             </nav>
         </header>
