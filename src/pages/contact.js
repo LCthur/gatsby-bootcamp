f76d0d87e7b96df9af8fa2e9fb1f7b944ca3f5a4
Loic Thurre <loic.thurre@gmail.com>
2019-08-29 09:02:38 +0200

    first challenge done to create 3 pages with pure react component that work

---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact = () => {
+    return(
+        <div>
+            <h1>Me contacter</h1>
+            <p>Tel : 078.100.10.10</p>
+            <p>email: loic.thurre@gmail.com</p>
+        </div>
+    )
+}
+
+export default Contact
